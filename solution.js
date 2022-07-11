cfd1afd652cfd85364332331b84552f223796092
Danica Yener <ny21037@googlemail.com>
2022-07-11 14:56:09 +0200

    2 aufgabe

---
+++ solution.js
@@ -0,0 +1,25 @@
+const isDog = true ;
+console.log(isDog == true ? "pat, pat" : "find me a dog to pat");
+const speedLimit = 50;
+const yourSpeed = 49;
+console.log(yourSpeed > speedLimit ? 'you are going to fast!': "you are driving beow the speed limit, oma");
+
+const age = 30
+console.log( age < 16 ? 'serve butter Beere' : 'serve beer');
+
+const isStudent = false;
+console.log(isStudent == true ? 'ticket consts 5.00€' : 'ticked costs 12.00€');
+
+const okMarie = 'nocake'
+console.log(okMarie == 'nocake' ? 'let them eat cake' : 'oh, bother');
+
+
+const num1 = 30
+const num2 = 939
+const num3 = 40.9
+
+console.log(num1 % 2 === 0 ? `${num1} is even`: `${num1} is odd`);
+
+console.log(num2 % 2 === 0 ? `${num1} is even`: `${num2} is odd`);
+
+console.log(num3 % 2 === 0 ? `${num1} is even`: `${num3} is odd`);
